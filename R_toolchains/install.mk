9acc28109d88edb9b88b92c580fb4f321616877a
nobodyxu <nobodyxu@lolol.me>
2019-11-24 00:21:11 +1100

    Add R_toolchains/

---
+++ R_toolchains/install.mk
@@ -0,0 +1,8 @@
+all: install_R_toolchains
+
+install_R_mirror:
+	apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
+	echo 'deb http://cloud.r-project.org/bin/linux debian buster-cran35' >/etc/apt/sources.list.d/R_backport.list
+
+install_R_toolchains: install_R_mirror
+	$(apt) install -y r-base-dev r-recommended libatlas3-base
