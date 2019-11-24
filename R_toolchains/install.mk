86f4fef8f28627703edbfe2c3ee965f2a8b52227
nobodyxu <nobodyxu@lolol.me>
2019-11-24 16:26:34 +1100

    Fix insecure repo warning by not checking

--- R_toolchains/install.mk
+++ R_toolchains/install.mk
@@ -2,7 +2,7 @@ all: install_R_toolchains
 
 install_R_mirror: install_gnupg
 	apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
-	echo 'deb http://cloud.r-project.org/bin/linux debian buster-cran35' >/etc/apt/sources.list.d/R_backport.list
+	echo 'deb allow-insecure=yes http://cloud.r-project.org/bin/linux debian buster-cran35' >/etc/apt/sources.list.d/R_backport.list
 
 
 install_R_toolchains: install_R_mirror
