a10dd4d8b500fd7b7eb1a0c1f79bd194cf2bf4c0
nobodyxu <nobodyxu@lolol.me>
2019-11-24 14:42:57 +1100

    Modify java/ for commit 4e87e4e

--- java/install.mk
+++ java/install.mk
@@ -1,6 +1,3 @@
 install_java:
 	$(apt) install -y default-jre default-jdk gradle
-	# Install gradle completion
-	mkdir -p ${HOME}/bash_completion.d
-	curl -LA gradle-completion https://edub.me/gradle-completion-bash -o ${HOME}/bash_completion.d/gradle-completion.bash
-	echo 'source ${HOME}/bash_completion.d/gradle-completion.bash' >> ~/.bashrc
+	$(su_orig) ./java/install_gradle_completion.sh
---
+++ java/install_gradle_completion.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p ${HOME}/bash_completion.d
+curl -LA gradle-completion https://edub.me/gradle-completion-bash -o ${HOME}/bash_completion.d/gradle-completion.bash
+echo 'source ${HOME}/bash_completion.d/gradle-completion.bash' >> ~/.bashrc
