dc708bac76bb7b120669cc12541cc1f2c70ad343
nobodyxu <nobodyxu@lolol.me>
2019-11-24 14:40:29 +1100

    Modify python3/ for commit 4e87e4e

--- python3/install.mk
+++ python3/install.mk
@@ -3,4 +3,4 @@ all: install_python
 install_python:
 	# Nodejs and npm are requirements for jupyterlab
 	$(apt) install -y python3 python3-pip python3-setuptools curl nodejs npm
-	pip3 install --user jupyterlab numpy pandas sklearn bokeh matlab modin[all]
+	$(su_orig) pip3 install --user jupyterlab numpy pandas sklearn bokeh matlab modin[all]
