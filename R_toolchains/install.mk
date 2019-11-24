f71c82e078028f6d1c6fa37cc0ac27a1cb4f2b23
nobodyxu <nobodyxu@lolol.me>
2019-11-24 21:52:25 +1100

    Revert commit 86f4f3f8 as it cause apt to emit warnings

--- R_toolchains/install.mk
+++ R_toolchains/install.mk
@@ -2,8 +2,8 @@ all: install_R_toolchains
 
 install_R_mirror: install_gnupg
 	apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
-	echo 'deb allow-insecure=yes http://cloud.r-project.org/bin/linux debian buster-cran35' >/etc/apt/sources.list.d/R_backport.list
+	echo 'deb http://cloud.r-project.org/bin/linux debian buster-cran35' >/etc/apt/sources.list.d/R_backport.list
 
 
 install_R_toolchains: install_R_mirror
-	$(apt) install -y r-base-dev r-recommended libatlas3-base
+	$(apt) install -y r-base-dev r-recommended libatlas3-base curl
