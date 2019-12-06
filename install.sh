8a1127903606212b61f6ce436a5948338a750ea6
nobodyxu <Jiahao_XU@outlook.com>
2019-12-06 13:37:25 +1100

    Add environmental variable ORIG_HOME.
    So that configuration that need to be placed in home dir can be done
    easily in Makefile.

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 [ -z "$ORIG_USER" ] && ORIG_USER=$USER
+[ "$ORIG_USER" = "root" ] && ORIG_HOME=/root || ORIG_HOME=/home/$ORIG_USER
 
-exec sudo env ORIG_USER=$ORIG_USER make $1
+exec sudo env ORIG_HOME=$ORIG_HOME ORIG_USER=$ORIG_USER make $1
