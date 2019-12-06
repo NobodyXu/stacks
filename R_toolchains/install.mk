448beafc57a2e4d96a5bb8554282bd351005763e
nobodyxu <Jiahao_XU@outlook.com>
2019-12-06 13:11:11 +1100

    Auto-detect Ncpus in Rprofile

--- R_toolchains/install.mk
+++ R_toolchains/install.mk
@@ -7,5 +7,5 @@ install_R_mirror: install_gnupg
 
 install_R_toolchains: install_R_mirror
 	$(apt) install -y r-base-dev r-recommended libatlas3-base curl libcurl4-openssl-dev libxml2-dev
-	cp ./R_toolchains/Rprofile ~/.Rprofile
+	$(su_orig) cp ./R_toolchains/Rprofile ~/.Rprofile
 	$(su_orig) ./R_toolchains/R_inst_packs.sh tidyverse data.table stringr stringi devtools
