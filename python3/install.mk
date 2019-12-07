241a12cec26b990019ef01ac086907aa137725b5
nobodyxu <Jiahao_XU@outlook.com>
2019-12-07 20:53:05 +1100

    Add pip3_upgrade as part of the stacks of python3

--- python3/install.mk
+++ python3/install.mk
@@ -4,3 +4,4 @@ install_python:
 	# Nodejs and npm are requirements for jupyterlab
 	sudo $(apt) install -y python3 python3-pip python3-setuptools curl nodejs npm
 	pip3 install --user jupyterlab numpy pandas sklearn bokeh matlab modin[all]
+	sudo cp ./python3/pip3_upgrade /usr/local/bin/
