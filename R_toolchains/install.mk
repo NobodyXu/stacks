ae17a538c4c37dde814a17ba5d4860bc659ee0a8
nobodyxu <Jiahao_XU@outlook.com>
2019-12-12 16:09:20 +1100

    Install additional apt packages for R
     - texlive-full for building Rmarkdown
     - build-essential and r-base-core for building packages that requires
     compile.

--- R_toolchains/install.mk
+++ R_toolchains/install.mk
@@ -4,7 +4,7 @@ install_R_mirror: install_gnupg
 	sudo ./R_toolchains/add_repo.sh
 
 install_R_toolchains: install_R_mirror install_curl
-	sudo $(apt) install -y r-base-dev r-recommended libatlas3-base libcurl4-openssl-dev libxml2-dev
+	sudo $(apt) install -y r-base-dev texlive-full build-essential r-base-core r-recommended libatlas3-base libcurl4-openssl-dev libxml2-dev
 	#$(cp_to_home) ./R_toolchains/Rprofile .Rprofile
 	cp ./R_toolchains/Rprofile ~/.Rprofile
 	./R_toolchains/R_inst_packs.sh tidyverse data.table stringr stringi devtools
