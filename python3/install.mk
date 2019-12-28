a9776404d92def576ab2fe7010f73ecfb8255259
Jiahao XU <Jiahao_XU@outlook.com>
2019-12-28 00:56:14 -0800

    Add python packages to PATH.

--- python3/install.mk
+++ python3/install.mk
@@ -3,5 +3,7 @@ all: install_python3
 install_python3:
 	# Nodejs and npm are requirements for jupyterlab
 	sudo $(apt) install -y python3 python3-pip python3-setuptools curl nodejs npm
+	# Use $$ to express $ (escape)
+	$(append) 'export PATH=$${HOME}/.local/bin:$${PATH}' ~/.bashrc
 	pip3 install --user jupyterlab numpy pandas sklearn bokeh matlab modin[all]
 	sudo cp ./python3/pip3_upgrade /usr/local/bin/
