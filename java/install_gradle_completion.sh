2f41ba1651dec06cb7b032ac8629bfa2726d2704
Jiahao XU <Jiahao_XU@outlook.com>
2019-12-28 00:55:38 -0800

    Add gradle completion plugin using append.sh
    So that when the script is ran for more than once, the plugin won't be
    loaded multiple times.

--- java/install_gradle_completion.sh
+++ java/install_gradle_completion.sh
@@ -2,4 +2,4 @@
 
 mkdir -p ${HOME}/bash_completion.d
 curl -LA gradle-completion https://edub.me/gradle-completion-bash -o ${HOME}/bash_completion.d/gradle-completion.bash
-echo 'source ${HOME}/bash_completion.d/gradle-completion.bash' >> ~/.bashrc
+./append.sh 'source ${HOME}/bash_completion.d/gradle-completion.bash' ~/.bashrc
