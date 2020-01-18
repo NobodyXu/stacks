1502f8f23252285a6a6852a67a01ea39a8c8ebff
nobodyxu <Jiahao_XU@outlook.com>
2020-01-18 15:26:30 +1100

    install_cling: install cpt.py into ~/.local/bin.

--- cling/run_cpt.sh
+++ cling/run_cpt.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-# Goto work dir
-cd ~/.local/src
-
 # Download cpt manager
+cd ~/.local/bin
 wget https://raw.githubusercontent.com/root-project/cling/master/tools/packaging/cpt.py
 chmod +x ./cpt.py
 
+# Goto work dir
+cd ~/.local/src
+
 # Check environment
-./cpt.py -c
+cpt.py -c
 
 # Start cpt manager to download source code and compile
-exec ./cpt.py --last-stable deb --create-dev-env Release -y --with-workdir=./cling-build/
+exec cpt.py --last-stable deb --create-dev-env Release -y --with-workdir=./cling-build/
