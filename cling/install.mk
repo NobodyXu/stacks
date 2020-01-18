f9d5fbf4d6cb5bf5f36d5ba04fa69ccf14c39805
nobodyxu <Jiahao_XU@outlook.com>
2020-01-18 23:06:04 +1100

    Add python as depency for cling

--- cling/install.mk
+++ cling/install.mk
@@ -3,6 +3,6 @@ all: install_cling
 make_local_src_dir:
 	mkdir -p ~/.local/src
 
-install_cling: make_local_src_dir install_c_toolchains install_git install_gnupg install_cmake
+install_cling: make_local_src_dir install_python install_c_toolchains install_git install_gnupg install_cmake
 	sudo $(apt) install -y debhelper devscripts
 	exec ./cling/run_cpt.sh
