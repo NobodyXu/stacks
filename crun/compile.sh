086b602a7fbdd625d41f18aa59b58646d1cfe920
nobodyxu <Jiahao_XU@outlook.com>
2020-01-20 00:12:11 +1100

    Restrain crun to use the last stable release

--- crun/compile.sh
+++ crun/compile.sh
@@ -5,6 +5,10 @@ cd ~/.local/src
 
 git clone https://github.com/containers/crun.git
 cd crun
+# Switch to last stable release
+## Remove any tags starts with 'v*' as currently, they are
+## all very old release.
+git tag -l | sort -V | grep -v 'v' | tail -n 1
 
 ./autogen.sh && ./configure
 make -j $(nproc)
