2e9aac953f371581b923725d51fa12bfad6f7112
nobodyxu <Jiahao_XU@outlook.com>
2020-01-20 00:05:25 +1100

    Install more dependencies for compiling c projects.

--- c_toolchains/install.mk
+++ c_toolchains/install.mk
@@ -1,4 +1,4 @@
 all: install_c_toolchains
 
-install_c_toolchains: install_clang
-	sudo $(apt) install -y ctags build-essential bison
+install_c_toolchains: install_clang install_gcc
+	sudo $(apt) install -y make ctags build-essential bison autoconf automake libtool pkgconf
