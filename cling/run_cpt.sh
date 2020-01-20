3f65cfec158b942ec052c27361c44c82baba8954
nobodyxu <Jiahao_XU@outlook.com>
2020-01-20 13:29:52 +1100

    Fix error when installing cling on new machines
    By `mkdir -p ~/.local/bin`.

--- cling/run_cpt.sh
+++ cling/run_cpt.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Download cpt manager
+mkdir -p ~/.local/bin
 cd ~/.local/bin
 if [ ! -f cpt.py ]; then
     wget https://raw.githubusercontent.com/root-project/cling/master/tools/packaging/cpt.py
