0008d15ccd74210b553c23cebd4418d8eb389f96
nobodyxu <Jiahao_XU@outlook.com>
2020-01-20 12:28:59 +1100

    Add reference for instructions for apt-listchanges

--- apt-listchanges/install.mk
+++ apt-listchanges/install.mk
@@ -1,5 +1,7 @@
 all: install_apt-listchanges
 
 install_apt-listchanges:
+	# The following instructions are adapted from:
+	#     https://askubuntu.com/a/272433/772092
 	sudo $(apt) install -y apt-listchanges
 	sudo dpkg-reconfigure apt-listchanges
