372acb5f6f8896cb389fe2ed91c8c5d0e16d887d
nobodyxu <Jiahao_XU@outlook.com>
2020-01-20 13:32:29 +1100

    Install wget in cling/install.mk.
    In case a new debian/ubuntu machines do not have wget installed e.x.
    in docker debian:latest image.

--- cling/install.mk
+++ cling/install.mk
@@ -4,5 +4,5 @@ make_local_src_dir:
 	mkdir -p ~/.local/src
 
 install_cling: make_local_src_dir install_python install_c_toolchains install_git install_gnupg
-	sudo $(apt) install -y debhelper devscripts
+	sudo $(apt) install -y debhelper devscripts wget
 	exec ./cling/run_cpt.sh
