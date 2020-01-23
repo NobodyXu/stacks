adb1355046b0a1d557861f38a42b964cdc1070d4
nobodyxu <Jiahao_XU@outlook.com>
2020-01-23 16:48:11 +1100

    Add target configure_rootless_podman_env along
    with script in podman/ so that podman can be used
    rootlessly.

---
+++ podman/change-kernel-settings.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Loads system info
+. /etc/os-release
+
+# Allow ping for unprivileged users
+sudo cp ./podman/10-rootless-ping-for-container.conf /etc/sysctl.d/
+
+# Allow userns for unprivileged users
+if [ "${ID}" = "debian" ]; then
+    sudo cp ./podman/10-userns-for-unprivileged-in-debian.conf /etc/sysctl.d/
+fi
+
+# Apply these settings
+sudo sysctl --system
+
+# Allow overlay for unprivileged
+if [ "${ID}" = "debian" ] || [ "${NAME}" = "Pop!_OS"]; then
+	sudo modprobe overlay 'permit_mounts_in_userns=1'
+	sudo cp ./podman/unprivileged-overlayfs.conf /etc/modprobe.d/
+fi
--- podman/install.mk
+++ podman/install.mk
@@ -6,7 +6,11 @@ all: install_podman
 add_key_for_podman:
 	./podman/add_key.sh
 
-install_podman: add_key_for_podman
+configure_rootless_podman_env:
+	sudo $(apt) install -y slirp4netns fuse-overlayfs
+	./podman/change-kernel-settings.sh
+
+install_podman: add_key_for_podman configure_rootless_podman_env
 	sudo $(apt) install -y podman
 	sudo mkdir -p /etc/containers
 	echo -e "[registries.search]\nregistries = ['docker.io', 'quay.io']" | sudo tee /etc/containers/registries.conf
