38a61c21628000e4222c9651f7a6d2dc21951d7d
nobodyxu <Jiahao_XU@outlook.com>
2020-01-23 20:30:26 +1100

    Fix typo in podman/change-kernel-settings.sh

--- podman/change-kernel-settings.sh
+++ podman/change-kernel-settings.sh
@@ -15,7 +15,7 @@ fi
 sudo sysctl --system
 
 # Allow overlay for unprivileged
-if [ "${ID}" = "debian" ] || [ "${NAME}" = "Pop!_OS"]; then
+if [ "${ID}" = "debian" ] || [ "${NAME}" = "Pop!_OS" ]; then
 	sudo modprobe overlay 'permit_mounts_in_userns=1'
 	sudo cp ./podman/unprivileged-overlayfs.conf /etc/modprobe.d/
 fi
