feab1b2a07f484b87d4ebf66d8d509bbb32bf091
nobodyxu <Jiahao_XU@outlook.com>
2020-01-23 16:51:32 +1100

    Add install_crun as dependency for install_podman

--- podman/install.mk
+++ podman/install.mk
@@ -10,7 +10,7 @@ configure_rootless_podman_env:
 	sudo $(apt) install -y slirp4netns fuse-overlayfs
 	./podman/change-kernel-settings.sh
 
-install_podman: add_key_for_podman configure_rootless_podman_env
+install_podman: add_key_for_podman configure_rootless_podman_env install_crun
 	sudo $(apt) install -y podman
 	sudo mkdir -p /etc/containers
 	echo -e "[registries.search]\nregistries = ['docker.io', 'quay.io']" | sudo tee /etc/containers/registries.conf
