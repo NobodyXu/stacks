e70bbe84f15ba9bf619196fd054df13d1f01b9af
nobodyxu <Jiahao_XU@outlook.com>
2020-01-26 19:51:40 +1100

    Change google-benchmark/run_google*.sh behavior
    from printing help when file not found to when not supplying optional
    argument and default file `./a.out` is not found or not executable.

--- google-benchmark/run_google-benchmarks.sh
+++ google-benchmark/run_google-benchmarks.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -ex
 
-[ -n "$1" ] && mybench=$1 || mybench=./a.out
-
-if [ ! -f $mybench ]; then
-    echo $0 [optional: executable linked with libbenchmark or libbenchmark_main]
-    echo If the optional arg is not provided, it is default to ./a.out.
-    exit 1
+if [ -n "$1" ]; then
+    mybench=$1
+else
+    mybench=./a.out
+    if [ ! -f $mybench ] || [ ! -x $mybench ]; then
+        echo $0 [optional: executable linked with libbenchmark or libbenchmark_main]
+        echo If the optional arg is not provided, it is default to ./a.out.
+        exit 1
+    fi
 fi
 
 sudo cpupower frequency-set --governor performance
