f425a445ced4737080a0a956b9b9532a43e2e806
nobodyxu <Jiahao_XU@outlook.com>
2020-01-26 18:09:53 +1100

    Use universal-ctags instead of ex-ctags

--- c_toolchains/install.mk
+++ c_toolchains/install.mk
@@ -1,4 +1,4 @@
 all: install_c_toolchains
 
 install_c_toolchains: install_clang install_gcc
-	sudo $(apt) install -y make ctags build-essential bison autoconf automake libtool pkgconf cmake
+	sudo $(apt) install -y make universal-ctags build-essential bison autoconf automake libtool pkgconf cmake
