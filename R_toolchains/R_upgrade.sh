c76dc5a3a16dd40c7cd8a9003c233b70ee3fabf7
nobodyxu <Jiahao_XU@outlook.com>
2020-02-01 12:41:10 +1100

    Add R_toolchains/R_upgrade.sh
    For ease of upgrading R packages and supply `Ncpus` automatically to
    speed up the upgrade process.

---
+++ R_toolchains/R_upgrade.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "update.packages(ask = F, Ncpus = `nproc`)" | R --no-save --no-restore
--- R_toolchains/install.mk
+++ R_toolchains/install.mk
@@ -7,4 +7,5 @@ install_R_toolchains: install_R_mirror install_curl
 	sudo $(apt) install -y r-base-dev texlive-full build-essential r-base-core r-recommended libatlas3-base libcurl4-openssl-dev libxml2-dev
 	#$(cp_to_home) ./R_toolchains/Rprofile .Rprofile
 	cp ./R_toolchains/Rprofile ~/.Rprofile
+	sudo cp ./R_toolchains/R_upgrade.sh /usr/local/bin/
 	./R_toolchains/R_inst_packs.sh tidyverse data.table stringr stringi devtools
