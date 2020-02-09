31a8a7e55c0f68505fd2414de1cfd31f251be1ec
nobodyxu <Jiahao_XU@outlook.com>
2020-02-10 02:38:27 +1100

    Create install_python3_basics
    which only install python3, pip, setuptools and curl.

--- python3/install.mk
+++ python3/install.mk
@@ -1,8 +1,10 @@
 all: install_python3
 
-install_python3:
-	# Nodejs and npm are requirements for jupyterlab
-	sudo $(apt) install -y python3 python3-pip python3-setuptools curl nodejs npm
+install_python3_basics:
+	sudo $(apt) install -y python3 python3-pip python3-setuptools curl
+
+install_python3: install_python3_basics
+	sudo $(apt) install -y nodejs npm
 	# Use $$ to express $ (escape)
 	$(append) 'export PATH=$${HOME}/.local/bin:$${PATH}' ~/.bashrc
 	pip3 install --user jupyterlab numpy pandas sklearn bokeh matlab modin[all]
