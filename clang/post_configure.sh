5dd9f8f833796e137bfbd9e0b24ca4461156eda9
nobodyxu <Jiahao_XU@outlook.com>
2020-02-10 20:42:45 +1100

    Fix update-alternatives in clang/post_configure.sh
    Set alternative /usr/bin/cpp to /usr/bin/clang instead of
    /usr/bin/clang++.

--- clang/post_configure.sh
+++ clang/post_configure.sh
@@ -6,4 +6,4 @@ sudo ln -f $(which ld.lld) /usr/bin/ld
 # Use clang as default compiler
 sudo update-alternatives --install /usr/bin/cc  cc  $(which clang)   100
 sudo update-alternatives --install /usr/bin/c++ c++ $(which clang++) 100
-sudo update-alternatives --install /usr/bin/cpp cpp $(which clang++) 100
+sudo update-alternatives --install /usr/bin/cpp cpp $(which clang) 100
