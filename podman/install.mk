2ace89daac3be31fe866ec7f5e3cd139e8bf8fc2
nobodyxu <Jiahao_XU@outlook.com>
2020-02-11 22:28:49 +1100

    Install fuse-overlay using install_fuse-overlayfs

--- podman/install.mk
+++ podman/install.mk
@@ -7,10 +7,10 @@ add_key_for_podman: install_apt-add-repository
 	./podman/add_key.sh
 
 configure_rootless_podman_env:
-	sudo $(apt) install -y slirp4netns fuse-overlayfs
+	sudo $(apt) install -y slirp4netns
 	./podman/change-kernel-settings.sh
 
-install_podman: add_key_for_podman configure_rootless_podman_env install_crun
+install_podman: add_key_for_podman configure_rootless_podman_env install_crun install_fuse-overlayfs
 	sudo $(apt) install -y podman
 	sudo mkdir -p /etc/containers
 	echo -e "[registries.search]\nregistries = ['docker.io', 'quay.io']" | sudo tee /etc/containers/registries.conf
