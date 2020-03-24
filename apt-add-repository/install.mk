c6fad5871614262003807739d4374271aba0a89e
nobodyxu <Jiahao_XU@outlook.com>
2020-03-24 19:57:03 +1100

    Add missing dependency for apt-add-repository

--- apt-add-repository/install.mk
+++ apt-add-repository/install.mk
@@ -2,4 +2,4 @@ all: install_apt-add-repository
 
 install_apt-add-repository: install_curl
 	sudo apt-get update
-	sudo apt-get install -y apt-transport-https ca-certificates gnupg2 gnupg-agent software-properties-common
+	sudo apt-get install -y apt-transport-https ca-certificates gnupg2 gnupg-agent software-properties-common dirmngr
