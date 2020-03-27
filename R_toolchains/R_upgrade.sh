5b4aee679851e54e4d331dfdd26427b8ab489b6f
nobodyxu <Jiahao_XU@outlook.com>
2020-03-27 17:32:19 +1100

    Always update packages installed by devtools
    without prompt

--- R_toolchains/R_upgrade.sh
+++ R_toolchains/R_upgrade.sh
@@ -3,4 +3,4 @@
 exec R --no-save --no-restore -e "
 library('devtools');
 update.packages(lib.loc = Sys.getenv('R_LIBS_USER'), ask = F, Ncpus = getOption('Ncpus'))
-devtools::update_packages()"
+devtools::update_packages(upgrade = 'always')"
