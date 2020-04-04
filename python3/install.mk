6484ab36450625daed571955f7598f52e84b4b8b
nobodyxu <Jiahao_XU@outlook.com>
2020-04-04 11:41:39 +1100

    Add install_c_toolchains as dep for python3

--- python3/install.mk
+++ python3/install.mk
@@ -3,7 +3,7 @@ all: install_python3
 install_python3_basics: install_curl
 	sudo $(apt) install -y python3 python3-pip python3-setuptools
 
-install_python3: install_python3_basics
+install_python3: install_python3_basics install_c_toolchains
 	sudo $(apt) install -y nodejs npm
 	# Use $$ to express $ (escape)
 	$(append) 'export PATH=$${HOME}/.local/bin:$${PATH}' ~/.bashrc
