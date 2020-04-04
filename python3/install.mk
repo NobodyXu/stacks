52980b5458631548435935926b7fe3a9573488f3
nobodyxu <Jiahao_XU@outlook.com>
2020-04-04 11:45:55 +1100

    Add dep install_c_toolchains for install_python3_basics

--- python3/install.mk
+++ python3/install.mk
@@ -1,11 +1,11 @@
 all: install_python3
 
-install_python3_basics: install_curl
+install_python3_basics: install_curl install_c_toolchains
 	sudo $(apt) install -y python3 python3-pip python3-setuptools
 	$(append) 'export PATH=$${HOME}/.local/bin:$${PATH}' ~/.bashrc
 	sudo cp ./python3/pip3_upgrade /usr/local/bin/
 
-install_python3: install_python3_basics install_c_toolchains
+install_python3: install_python3_basics
 	sudo $(apt) install -y nodejs npm
 	# Use $$ to express $ (escape)
 	pip3 install --user jupyterlab numpy pandas sklearn bokeh matlab modin[all]
