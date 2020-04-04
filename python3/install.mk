7877d968c42cab248452851a6ff44bd4ad5600f5
nobodyxu <Jiahao_XU@outlook.com>
2020-04-04 11:43:25 +1100

    Refactor python3 stack
    Make install_python3 a target for additional plugins, not configuring
    the system so that plugins can run or installing pip3_upgrade.

--- python3/install.mk
+++ python3/install.mk
@@ -2,10 +2,10 @@ all: install_python3
 
 install_python3_basics: install_curl
 	sudo $(apt) install -y python3 python3-pip python3-setuptools
+	$(append) 'export PATH=$${HOME}/.local/bin:$${PATH}' ~/.bashrc
+	sudo cp ./python3/pip3_upgrade /usr/local/bin/
 
 install_python3: install_python3_basics install_c_toolchains
 	sudo $(apt) install -y nodejs npm
 	# Use $$ to express $ (escape)
-	$(append) 'export PATH=$${HOME}/.local/bin:$${PATH}' ~/.bashrc
 	pip3 install --user jupyterlab numpy pandas sklearn bokeh matlab modin[all]
-	sudo cp ./python3/pip3_upgrade /usr/local/bin/
