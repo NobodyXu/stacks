16644888f3ecad4564571601ed23d1d50058a985
nobodyxu <Jiahao_XU@outlook.com>
2020-04-06 00:29:23 +1000

    Change pull.rebase to preserve
    to preserve locally-made git merges.

--- git/install.mk
+++ git/install.mk
@@ -4,4 +4,4 @@ install_git:
 	sudo $(apt) install -y git ca-certificates git-lfs
 	git config --global merge.ff false
 	git config --global pull.ff only
-	git config --global pull.rebase true
+	git config --global pull.rebase preserve
