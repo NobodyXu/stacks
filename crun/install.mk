2e1b7b7c6a5ff46b5459579b2bcd634d635e13ec
nobodyxu <Jiahao_XU@outlook.com>
2020-04-15 18:30:44 +1000

    Replace dep install_python3 with install_python3_basics

--- crun/install.mk
+++ crun/install.mk
@@ -1,5 +1,5 @@
 all: install_crun
 
-install_crun: install_git install_c_toolchains install_python3
+install_crun: install_git install_c_toolchains install_python3_basics
 	sudo $(apt) install -y libsystemd-dev libcap-dev libseccomp-dev libyajl-dev go-md2man
 	./crun/compile.sh
