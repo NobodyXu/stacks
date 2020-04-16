94b1695392bfac1c3e74bddba51b6ddf3bcd18b8
nobodyxu <Jiahao_XU@outlook.com>
2020-04-17 09:33:32 +1000

    Install fuse-overlayfs from podman repository
    as it is new enough.

--- fuse-overlayfs/install.mk
+++
@@ -1,5 +0,0 @@
-all: install_fuse-overlayfs
-
-install_fuse-overlayfs: install_git install_c_toolchains
-	sudo $(apt) install -y libc6-dev pkgconf libfuse3-dev
-	./fuse-overlayfs/compile.sh
--- podman/install.mk
+++ podman/install.mk
@@ -11,7 +11,7 @@ configure_rootless_podman_env:
 	./podman/change-kernel-settings.sh
 
 install_podman: add_key_for_podman configure_rootless_podman_env install_crun install_fuse-overlayfs
-	sudo $(apt) install -y podman
+	sudo $(apt) install -y podman fuse-overlayfs
 	sudo mkdir -p /etc/containers
 	echo -e "[registries.search]\nregistries = ['docker.io', 'quay.io']" | sudo tee /etc/containers/registries.conf
 	# Configure podman
