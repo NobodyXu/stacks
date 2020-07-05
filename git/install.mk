669a0563364e4c047fc7017b0ce93acd299b3e1d
nobodyxu <Jiahao_XU@outlook.com>
2020-07-05 12:41:18 +1000

    No install recommends when installing git

--- git/install.mk
+++ git/install.mk
@@ -1,7 +1,7 @@
 all: install_git
 
 install_git:
-	sudo $(apt) install -y git ca-certificates git-lfs
+	sudo $(apt) install -y git ca-certificates git-lfs --no-install-recommends
 	git config --global merge.ff false
 	git config --global pull.ff only
 	git config --global pull.rebase preserve
