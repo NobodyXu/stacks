991e922686a5c865e60a4bae8edd8c5d50eb703e
nobodyxu <Jiahao_XU@outlook.com>
2020-07-05 12:00:28 +1000

    Install crun from libpod dev source
    instead of compilation

--- podman/install.mk
+++ podman/install.mk
@@ -10,8 +10,8 @@ configure_rootless_podman_env:
 	sudo $(apt) install -y slirp4netns
 	./podman/change-kernel-settings.sh
 
-install_podman: add_key_for_podman configure_rootless_podman_env install_crun
-	sudo $(apt) install -y podman fuse-overlayfs
+install_podman: add_key_for_podman configure_rootless_podman_env
+	sudo $(apt) install -y podman fuse-overlayfs crun
 	sudo mkdir -p /etc/containers
 	echo -e "[registries.search]\nregistries = ['docker.io', 'quay.io']" | sudo tee /etc/containers/registries.conf
 	# Configure podman
