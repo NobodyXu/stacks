f3f38944597a261506567034edd86c1b43b15450
nobodyxu <Jiahao_XU@outlook.com>
2020-07-11 11:38:54 +1000

    Remove rebase.merge option as not valid

--- git/install.mk
+++ git/install.mk
@@ -4,4 +4,3 @@ install_git:
 	sudo $(apt) install -y git ca-certificates git-lfs --no-install-recommends
 	git config --global merge.ff false
 	git config --global pull.ff only
-	git config --global pull.rebase merge
