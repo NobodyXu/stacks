aea7aa1c231b112d9e10700cbcd05b510b125b7e
nobodyxu <Jiahao_XU@outlook.com>
2020-11-21 19:16:54 +1100

    Add target install_clang_tools

--- clang/install.mk
+++ clang/install.mk
@@ -1,5 +1,8 @@
-all: install_clang
+all: install_clang install_clang_tools
 
 install_clang: install_gcc
 	sudo $(apt) install -y $(shell ./clang/check_and_return_packs.sh)
 	./clang/post_configure.sh
+
+install_clang_tools: install_clang
+	sudo $(apt) install -y clang-tools clang-tidy
